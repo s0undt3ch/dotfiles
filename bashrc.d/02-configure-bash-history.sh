89229cda9ad9e81080d911ae1cc12ce9c955c139
Pedro Algarvio <pedro@algarvio.me>
2013-04-17 00:30:04 +0100

    Update bash history settings + bash settings.

--- bashrc.d/02-configure-bash-history.sh
+++ bashrc.d/02-configure-bash-history.sh
@@ -1,12 +1,19 @@
 # don't put duplicate lines in the history. See bash(1) for more options
 # don't overwrite GNU Midnight Commander's setting of `ignorespace'.
-export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
+#export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 # ... or force ignoredups and ignorespace
-export HISTCONTROL=ignoreboth::ignorespace
-export HISTFILESIZE=1000000000
-export HISTSIZE=1000000000
+export HISTCONTROL=ignoreboth
+export HISTFILESIZE=100000000
+export HISTSIZE=100000000
 
-# append to the history file, don't overwrite it
+
+# auto correct the case on tab completion
+shopt -s nocaseglob
+
+# Correct typos in your file/directory name
+shopt -s cdspell
+
+# append to the history file, don't overwrite it, even on multiple simultaneous shells
 shopt -s histappend
 
 # check the window size after each command and, if necessary,
