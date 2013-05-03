93155438305efd39e1aeb89ac89692d7e1a824bb
Pedro Algarvio <pedro@algarvio.me>
2013-05-03 18:52:14 +0100

    Differentiate root from regular users. Take into account that `__git_ps1` might not be available.

--- bashrc.d/99-configure-bash-prompt.sh
+++ bashrc.d/99-configure-bash-prompt.sh
@@ -1,7 +1,19 @@
+if [ `id -u` == '0' ]; then
+    USERCOLOR='01;34m'
+else
+    USERCOLOR='01;32m'
+fi
+
 #GIT_PS1_SHOWDIRTYSTATE=1
 #GIT_PS1_SHOWUPSTREAM="verbose"
 GIT_PS1_SHOWSTASHSTATE=1
 #GIT_PS1_SHOWUNTRACKEDFILES=1
+
+if [ "$(command -v __git_ps1)x" = "x" ]; then
+    __git_ps1() {
+        echo ""
+    }
+fi
 
 __hg_ps1() {
     hg prompt " (HG:{branch})" 2> /dev/null
@@ -13,7 +25,7 @@ PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*|screen*)
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]: \[\033[01;34m\]\w\[\e[1;33m\]$(__git_ps1 " (%s)")$(__hg_ps1)\[\e[1;34m\]\[\033[00m\]
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[${USERCOLOR}\u@\h\[\033[00m\]: \[\033[01;34m\]\w\[\e[1;33m\]$(__git_ps1 " (%s)")$(__hg_ps1)\[\e[1;34m\]\[\033[00m\]
 \$ '
     PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD}\007"'
     ;;
