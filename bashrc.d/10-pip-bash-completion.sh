a82c18d5ddaf3dd75b98f887e0432701def59be0
Pedro Algarvio <pedro@algarvio.me>
2013-08-26 10:26:44 +0000

    Don't export `PIP_DOWNLOAD_CACHE`, `PIP_SOURCE_DIR` and `PIP_BUILD_DIR`.

--- bashrc.d/10-pip-bash-completion.sh
+++ bashrc.d/10-pip-bash-completion.sh
@@ -1,8 +1,8 @@
 # pip bash completion start
 
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-export PIP_SOURCE_DIR=$HOME/.pip/source
-export PIP_BUILD_DIR=$HOME/.pip/build
+#export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+#export PIP_SOURCE_DIR=$HOME/.pip/source
+#export PIP_BUILD_DIR=$HOME/.pip/build
 export PIP_RESPECT_VIRTUALENV=true
 export PIP_REQUIRE_VIRTUALENV=true
 export VIRTUALENV_USE_DISTRIBUTE=1
