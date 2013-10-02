adb0226438dcb8cdf727d4f27a58978de2084292
Pedro Algarvio <pedro@algarvio.me>
2013-10-02 13:56:53 +0100

    Check against existing directory, not file.

--- bashrc.d/32-android-sdk.sh
+++ bashrc.d/32-android-sdk.sh
@@ -1,4 +1,4 @@
 # Android SDK Paths
-if [ -f ~/projects/.android-sdk ]; then
+if [ -d ~/projects/.android-sdk ]; then
     export PATH=${PATH}:~/projects/.android-sdk/tools:~/projects/.android-sdk/platform-tools
 fi
