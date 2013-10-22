49bf38f17f5dfb66c0d1c2d920a109aa4d5d3359
Pedro Algarvio <pedro@algarvio.me>
2013-10-22 12:09:32 +0100

    "Say" that we want/support 256 color terminals.

--- bashrc.d/90-environment-variables.sh
+++ bashrc.d/90-environment-variables.sh
@@ -1,3 +1,10 @@
 #!/bin/bash - 
 
 export LANGUAGE=en_GB:en
+
+# Enable 256 color capabilities for appropriate terminals
+
+# Set this variable in your local shell config (such as ~/.bashrc)
+# if you want remote xterms connecting to this system, to be sent 256 colors.
+# This must be set before reading global initialization such as /etc/bashrc.
+SEND_256_COLORS_TO_REMOTE=1
