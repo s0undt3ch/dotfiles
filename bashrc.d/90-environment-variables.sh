0b7d10a0757bc718403952efc9992f71b7ac9fa5
Pedro Algarvio <pedro@algarvio.me>
2013-10-22 12:10:54 +0100

    Export the environment variable.

--- bashrc.d/90-environment-variables.sh
+++ bashrc.d/90-environment-variables.sh
@@ -7,4 +7,4 @@ export LANGUAGE=en_GB:en
 # Set this variable in your local shell config (such as ~/.bashrc)
 # if you want remote xterms connecting to this system, to be sent 256 colors.
 # This must be set before reading global initialization such as /etc/bashrc.
-SEND_256_COLORS_TO_REMOTE=1
+export SEND_256_COLORS_TO_REMOTE=1
