365cd35a5fcf9a60a84fc82db47d4b2093cb2ea8
Pedro Algarvio <pedro@algarvio.me>
2014-08-10 21:03:42 +0100

    Add script to update libs submodules

---
+++ libs/update-libs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+UNTRACKED_FILES=$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)
+
+if [ $UNTRACKED_FILES -eq 1 ]; then
+    echo "The are files with uncommited changes. Please commit those first before updating the libs submodules."
+    exit 1
+fi
+
+SCRIPT_DIR=`cd $(dirname $0) && pwd`
+cd ${SCRIPT_DIR}
+
+LIBS=$(ls $SCRIPT_DIR)
+for lib in $LIBS; do
+    [ ! -d $lib ] && continue
+    echo "Processing $lib"
+    cd $lib
+    git pull -u
+    cd ../../
+    git commit -am "Updated to latest libs/$bundle"
+    cd $SCRIPT_DIR
+done
+exit
