a358964540ffd5960277de4f67253e4ff35dbe6a
Pedro Algarvio <pedro@algarvio.me>
2014-08-11 00:57:25 +0100

    We need to export the variables. Add completion.

--- bashrc.d/33-pyenv-support.sh
+++ bashrc.d/33-pyenv-support.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-PYENV_ROOT="$HOME/.Dotfiles/libs/pyenv"
-PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT="$HOME/.Dotfiles/libs/pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+# Completion
+source ${PYENV_ROOT}/completions/pyenv.bash
 
 eval "$(pyenv init -)"
