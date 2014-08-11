1aaa2bbf143b12d418770ebe2677505e51faa2f3
Pedro Algarvio <pedro@algarvio.me>
2014-08-11 15:47:59 +0100

    Comment out the PIP environment variables

--- bashrc.d/10-pip-bash-completion.sh
+++ bashrc.d/10-pip-bash-completion.sh
@@ -3,8 +3,8 @@
 #export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 #export PIP_SOURCE_DIR=$HOME/.pip/source
 #export PIP_BUILD_DIR=$HOME/.pip/build
-export PIP_RESPECT_VIRTUALENV=true
-export PIP_REQUIRE_VIRTUALENV=true
+#export PIP_RESPECT_VIRTUALENV=true
+#export PIP_REQUIRE_VIRTUALENV=true
 export VIRTUALENV_USE_DISTRIBUTE=1
 
 _pip_completion()
