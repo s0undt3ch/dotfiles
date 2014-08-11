38f270ddf9cfcabde5d60e4bdec17ec81468eeaa
Pedro Algarvio <pedro@algarvio.me>
2014-08-11 15:47:23 +0100

    Add `pyenv-virtualenv`

--- bashrc.d/33-pyenv-support.sh
+++ bashrc.d/33-pyenv-support.sh
@@ -2,8 +2,10 @@
 
 export PYENV_ROOT="$HOME/.Dotfiles/libs/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 
 # Completion
 source ${PYENV_ROOT}/completions/pyenv.bash
 
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
