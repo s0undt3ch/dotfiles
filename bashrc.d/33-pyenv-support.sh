f9bb1ac709c4609ab0f2050261cba1fa3ee5bec3
Pedro Algarvio <pedro@algarvio.me>
2014-08-11 22:39:12 +0100

    Minor reordering

--- bashrc.d/33-pyenv-support.sh
+++ bashrc.d/33-pyenv-support.sh
@@ -4,8 +4,11 @@ export PYENV_ROOT="$HOME/.Dotfiles/libs/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 
-# Completion
-source ${PYENV_ROOT}/completions/pyenv.bash
-
+# Init pyenv
 eval "$(pyenv init -)"
+
+# Init pyenv virtualenv
 eval "$(pyenv virtualenv-init -)"
+
+# Completion
+. "${PYENV_ROOT}/completions/pyenv.bash"
