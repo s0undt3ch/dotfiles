a3de4192f6cf3ab1437e2d2a786b812b62d916fe
Pedro Algarvio <pedro@algarvio.me>
2014-08-13 19:56:26 +0100

    Make it conditional of pyenv being found

--- bashrc.d/33-pyenv-support.sh
+++ bashrc.d/33-pyenv-support.sh
@@ -4,17 +4,19 @@ export PYENV_ROOT="$HOME/.Dotfiles/libs/pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 
-# Init pyenv
-eval "$(pyenv init -)"
+if [ "$(which pyenv)" != "" ]; then
+    # Init pyenv
+    eval "$(pyenv init -)"
 
-# Start pyenv-virtualwrapper
-pyenv virtualenvwrapper
+    # Start pyenv-virtualwrapper
+    pyenv virtualenvwrapper
 
-# Init pyenv virtualenv
-eval "$(pyenv virtualenv-init -)"
+    # Init pyenv virtualenv
+    eval "$(pyenv virtualenv-init -)"
 
-# Completion
-. "${PYENV_ROOT}/completions/pyenv.bash"
+    # Completion
+    . "${PYENV_ROOT}/completions/pyenv.bash"
 
-# Virtualenv auto-activation
-. "${PYENV_ROOT}/plugins/pyenv-autoenv/bin/pyenv-autoenv"
+    # Virtualenv auto-activation
+    . "${PYENV_ROOT}/plugins/pyenv-autoenv/bin/pyenv-autoenv"
+fi
