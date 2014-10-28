598515adada91b0c4dd253cb07bd6fd0cfb9a041
Pedro Algarvio <pedro@algarvio.me>
2014-10-28 07:12:35 +0000

    Float firefox so full screen flash works

--- tag-wm/config/awesome/rc.lua
+++ tag-wm/config/awesome/rc.lua
@@ -940,6 +940,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "KeePass2" },
       properties = { floating = true } },
+    { rule = { class = "Firefox" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
