eb315cc7e1cd371fe3acb2790625d2b0e0b049ae
Pedro Algarvio <pedro@algarvio.me>
2024-11-26 17:53:50 +0000

    Install `ai.copilot` extra

--- neovim/nvim/lua/config/lazy.lua
+++ neovim/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
