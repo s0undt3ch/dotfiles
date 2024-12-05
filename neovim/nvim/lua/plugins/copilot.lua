c07a1388590aa265826857af411e1197ab9a5534
Pedro Algarvio <pedro@algarvio.me>
2024-12-05 17:54:34 +0000

    Copilot configuration

---
+++ neovim/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
